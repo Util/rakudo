42cbb57d621489be216d437d499aac8a996b33d5
Jonathan Worthington <jnthn@jnthn.net>
2009-10-10 17:26:04 +0200

    Re-work binder into a library of its own, which the dynops and dynpmcs both link against.

--- src/binder/bind.c
+++ src/binder/bind.c
@@ -4,12 +4,14 @@ Copyright (C) 2009, The Perl Foundation.
 */
 
 #include "parrot/parrot.h"
-#include "sigguts.h"
+#undef PARROT_IN_EXTENSION
+#include "bind.h"
 
 /* Takes a signature along with positional and named arguments and binds them
  * into the provided lexpad (actually, anything that has a Hash interface will
  * do). Returns 0 if binding fails, and non-zero otherwise. */
 INTVAL
+PARROT_EXPORT
 Rakudo_binding_bind_signature(PARROT_INTERP, PMC *lexpad, PMC *signature,
                                PMC *pos_args, PMC *named_args,
                                INTVAL no_nom_type_check, STRING **error) {
